399885fb776b654ee1d9bc8ad9f1ce2b8c5dcc02
Regi Adi <regiadi.priyanto@gmail.com>
2024-12-19 14:13:23 +0700

    Refactor game object to struct

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,155 @@
 use macroquad::prelude::*;
+use nalgebra::Vector2;
 use rapier2d::prelude::*;
 
-//const MOVEMENT_SPEED: f32 = 10.0;
+const BALL_SIZE: f32 = 0.2;
+const BALL_RESTITUTION: f32 = 1.7;
+const GROUND_SIZE: Vec2 = vec2(0.5, 0.1);
+
+struct Game {
+    rigid_body_set: RigidBodySet,
+    collider_set: ColliderSet,
+    gravity: Vector2<f32>,
+    integration_parameters: IntegrationParameters,
+    physics_pipeline: PhysicsPipeline,
+    island_manager: IslandManager,
+    broad_phase: DefaultBroadPhase,
+    narrow_phase: NarrowPhase,
+    impulse_joint_set: ImpulseJointSet,
+    multibody_joint_set: MultibodyJointSet,
+    ccd_solver: CCDSolver,
+    query_pipeline: QueryPipeline,
+    physics_hooks: (),
+    event_handler: (),
+    camera: Camera2D,
+}
+
+struct Ball {
+    size: f32,
+    collider: Collider,
+    rigid_body: RigidBody,
+    rigid_body_handle: Option<RigidBodyHandle>,
+}
+
+struct Ground {
+    size: Vec2,
+    collider: Collider,
+    collider_handle: Option<ColliderHandle>,
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    //let mut x = screen_width() / 2.0;
-    //let mut y = screen_height() / 2.0;
+    let mut game = Game {
+        rigid_body_set: RigidBodySet::new(),
+        collider_set: ColliderSet::new(),
+        gravity: vector![0.0, 9.81],
+        integration_parameters: IntegrationParameters::default(),
+        physics_pipeline: PhysicsPipeline::new(),
+        island_manager: IslandManager::new(),
+        broad_phase: DefaultBroadPhase::new(),
+        narrow_phase: NarrowPhase::new(),
+        impulse_joint_set: ImpulseJointSet::new(),
+        multibody_joint_set: MultibodyJointSet::new(),
+        ccd_solver: CCDSolver::new(),
+        query_pipeline: QueryPipeline::new(),
+        physics_hooks: (),
+        event_handler: (),
+        camera: Camera2D {
+            zoom: vec2(0.5, 0.5 * screen_width() / screen_height()),
+            target: vec2(1.0, 1.0),
+            ..Default::default()
+        }
+    };
 
-    let mut rigid_body_set = RigidBodySet::new();
-    let mut collider_set = ColliderSet::new();
+    let mut ball = Ball {
+        size: BALL_SIZE,
+        collider: ColliderBuilder::ball(BALL_SIZE).restitution(BALL_RESTITUTION).build(),
+        rigid_body: RigidBodyBuilder::dynamic()
+            .translation(vector![1.0, 0.1])
+            .build(),
+        rigid_body_handle: None,
+    };
+
+    let mut ground = Ground {
+        size: GROUND_SIZE,
+        collider: ColliderBuilder::cuboid(GROUND_SIZE.x, GROUND_SIZE.y)
+            .translation(vector![1.0, 2.0])
+            .build(),
+        collider_handle: None,
+    };
 
     // create the ground
-    let collider = ColliderBuilder::cuboid(100.0, 20.0)
-        .translation(vector![200.0, 500.0])
-        .build();
-    let ground_handle = collider_set.insert(collider);
+    ground.collider_handle = Some(game.collider_set.insert(ground.collider));
 
     // create another ground
     let collider = ColliderBuilder::cuboid(100.0, 20.0)
         .translation(vector![400.0, 400.0])
         .build();
-    let ground_handle_2 = collider_set.insert(collider);
+    let ground_handle_2 = game.collider_set.insert(collider);
 
-    // create the bouncing ball
+    ball.rigid_body_handle = Some(game.rigid_body_set.insert(ball.rigid_body));
+    game.collider_set.insert_with_parent(
+        ball.collider, 
+        ball.rigid_body_handle.expect("Empty RigidBodyHandle"), 
+        &mut game.rigid_body_set
+    );
+
+    // create another bouncing ball
     let rigid_body = RigidBodyBuilder::dynamic()
-        .translation(vector![150.0, 100.0])
+        .translation(vector![420.0, 100.0])
         .build();
-    let collider = ColliderBuilder::ball(30.0).restitution(1.0).build();
-    let ball_body_handle = rigid_body_set.insert(rigid_body);
-    collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
-
-    let rigid_body = RigidBodyBuilder::dynamic()
-        .translation(vector![450.0, 50.0])
-        .build();
-    let collider = ColliderBuilder::ball(50.0).restitution(1.5).build();
-    let ball_body_handle_2 = rigid_body_set.insert(rigid_body);
-    collider_set.insert_with_parent(collider, ball_body_handle_2, &mut rigid_body_set);
-
-    // create other structures necessary for the simulation
-    let gravity = vector![0.0, 100.0];
-    let integration_parameters = IntegrationParameters::default();
-    let mut physics_pipeline = PhysicsPipeline::new();
-    let mut island_manager = IslandManager::new();
-    let mut broad_phase = DefaultBroadPhase::new();
-    let mut narrow_phase = NarrowPhase::new();
-    let mut impulse_joint_set = ImpulseJointSet::new();
-    let mut multibody_joint_set = MultibodyJointSet::new();
-    let mut ccd_solver = CCDSolver::new();
-    let mut query_pipeline = QueryPipeline::new();
-    let physics_hooks = ();
-    let event_handler = ();
+    let collider = ColliderBuilder::ball(50.0).restitution(1.0).build();
+    let ball_body_handle_2 = game.rigid_body_set.insert(rigid_body);
+    game.collider_set.insert_with_parent(collider, ball_body_handle_2, &mut game.rigid_body_set);
 
     loop {
-        physics_pipeline.step(
-            &gravity,
-            &integration_parameters,
-            &mut island_manager,
-            &mut broad_phase,
-            &mut narrow_phase,
-            &mut rigid_body_set,
-            &mut collider_set,
-            &mut impulse_joint_set,
-            &mut multibody_joint_set,
-            &mut ccd_solver,
-            Some(&mut query_pipeline),
-            &physics_hooks,
-            &event_handler,
+        game.physics_pipeline.step(
+            &game.gravity,
+            &game.integration_parameters,
+            &mut game.island_manager,
+            &mut game.broad_phase,
+            &mut game.narrow_phase,
+            &mut game.rigid_body_set,
+            &mut game.collider_set,
+            &mut game.impulse_joint_set,
+            &mut game.multibody_joint_set,
+            &mut game.ccd_solver,
+            Some(&mut game.query_pipeline),
+            &game.physics_hooks,
+            &game.event_handler,
         );
 
-        let ball_body = &rigid_body_set[ball_body_handle];
-        let ball_body_2 = &rigid_body_set[ball_body_handle_2];
-        let ground = &collider_set[ground_handle];
-        let ground_2 = &collider_set[ground_handle_2];
-        //println!(
-        //    "Ball altitude: x:{} y:{}", 
-        //    ball_body.translation().x,
-        //    ball_body.translation().y
-        //);
+        ball.rigid_body = game.rigid_body_set[ball.rigid_body_handle.expect("Empty RigidBodyHandle")].clone();
+        let ball_body_2 = &game.rigid_body_set[ball_body_handle_2];
+        ground.collider = game.collider_set[ground.collider_handle.expect("Empty ColliderBodyHandle")].clone();
+        let ground_2 = &game.collider_set[ground_handle_2];
 
         clear_background(GRAY);
 
-        //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        //draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        // Render some primitives in camera space
+        set_camera(&game.camera);
 
         if is_key_down(KeyCode::Up) {
-            draw_text("UP ^", 100.0, 50.0, 100.0, RED
-            );
-            //y -= MOVEMENT_SPEED;
+            game.camera.target.y -= 0.05;  
         }
 
         if is_key_down(KeyCode::Down) {
             draw_text("DOWN ^", 100.0, 50.0, 100.0, RED);
-            //y += MOVEMENT_SPEED;
+            game.camera.target.y += 0.05;  
         }
 
         if is_key_down(KeyCode::Right) {
-            draw_text(
-                "RIGHT ->", 
-                screen_width() / 2.0,
-                screen_height() / 2.0, 
-                100.0, 
-                RED
-            );
-
-            //x += MOVEMENT_SPEED;
+            game.camera.target.x += 0.05;  
         }
 
         if is_key_down(KeyCode::Left) {
-            draw_text(
-                "LEFT <-", 
-                screen_width() / 2.0,
-                screen_height() / 2.0, 
-                100.0, 
-                RED
-            );
-            //x -= MOVEMENT_SPEED;
+            game.camera.target.x -= 0.05;  
         }
 
         draw_circle(
-            ball_body.translation().x, 
-            ball_body.translation().y, 
-            30.0, 
+            ball.rigid_body.translation().x, 
+            ball.rigid_body.translation().y, 
+            ball.size, 
             RED
         );
 
@@ -134,10 +161,10 @@ async fn main() {
         );
 
         draw_rectangle(
-            ground.translation().x - 100.0, 
-            ground.translation().y - 20.0, 
-            100.0 * 2.0, 
-            20.0 * 2.0, 
+            ground.collider.translation().x - ground.size.x, 
+            ground.collider.translation().y - ground.size.y, 
+            ground.size.x * 2.0, 
+            ground.size.y * 2.0, 
             GREEN
         );
 
@@ -148,6 +175,9 @@ async fn main() {
             20.0 * 2.0, 
             BLUE
         );
+
+        set_default_camera();
+        draw_text("HELLO", 30.0, 200.0, 30.0, BLACK);
 
         next_frame().await;
     }
